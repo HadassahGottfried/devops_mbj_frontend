d6592a6b48088718d501307d38b89af7b8c8116f
HadassahGottfried <Hadasa3281@gmail.com>
2024-12-11 04:08:15 +0200

    Automated commit message

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Welcome to the Hadassah Gottfried React App</h1>
-        <button onClick={fetchData()} className="fetch-button">Fetch backend</button>
+        <button onClick={fetchData} className="fetch-button">Fetch backend</button>
         <p>{message}</p>
       </header>
     </div>
